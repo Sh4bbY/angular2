640ca70ba9ebdfdc5380b0bcc4ac115080d38f9f
Sascha Bialon <sascha.bialon@shabbtech.com>
2017-04-13 01:42:58 +0200

    refactor(appComonents): refactored app code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,17 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule}  from '@angular/platform-browser';
-import {AppComponent} from './app.component';
+import { NgModule } from '@angular/core';
+import { BrowserModule }  from '@angular/platform-browser';
+import { AppComponent } from './components/app/app.component';
+import { routing }   from './app.routing';
+import { HomeComponent } from './components/home.component';
 
 @NgModule({
     imports     : [
         BrowserModule,
+        routing,
     ],
     declarations: [
         AppComponent,
+        HomeComponent,
     ],
     bootstrap   : [AppComponent],
 })
