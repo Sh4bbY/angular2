cd99a1b9fd9125372d1fd0fa48d36961c93e8d00
Sascha Bialon <sascha.bialon@shabbtech.com>
2017-04-19 19:03:04 +0200

    chore(bootstrap): removed vendor chunk and placed in back to the bootstap chunk

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,3 +1,15 @@
+import 'core-js/es6';
+import 'core-js/es7/reflect';
+require('zone.js/dist/zone');
+import '@angular/platform-browser';
+import '@angular/platform-browser-dynamic';
+import '@angular/core';
+import '@angular/common';
+import '@angular/http';
+import '@angular/router';
+import 'rxjs';
+import 'hammerjs';
+
 // prevents error message about md-theme is not loaded with webpack-dev-server
 if (process.env.NODE_ENV === 'development') {
     require('@angular/material/prebuilt-themes/indigo-pink.css');
